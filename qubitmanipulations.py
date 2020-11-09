d30c8ccddffe827fad5a29544bf367b60185c961
aapoorv <abhyudayaapoorvacr7@gmail.com>
2020-11-10 01:41:50 +0530

    Added a test python file

---
+++ qubitmanipulations.py
@@ -0,0 +1,12 @@
+from qiskit import *
+from math import pi
+from qiskit.visualization import plot_bloch_multivector
+
+#X-gate
+qc = QuantumCircuit(1)
+qc.x(0)
+print(qc.draw('mpl'))
+
+# backend  = Aer.get_backend('statevector_simulator')
+# out = execute(qc, backend).result().get_statevector()
+# print(plot_bloch_multivector(out))
