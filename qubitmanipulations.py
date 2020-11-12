ef43ade61cffae589f5973434983896f93b84204
aapoorv <abhyudayaapoorvacr7@gmail.com>
2020-11-12 22:03:10 +0530

    Added Hadamard Gate

--- qubitmanipulations.py
+++ qubitmanipulations.py
@@ -5,8 +5,12 @@ from qiskit.visualization import plot_bloch_multivector
 #X-gate
 qc = QuantumCircuit(1)
 qc.x(0)
-print(qc.draw('mpl'))
+print(qc.draw('text'))
 
 # backend  = Aer.get_backend('statevector_simulator')
 # out = execute(qc, backend).result().get_statevector()
 # print(plot_bloch_multivector(out))
+
+#hadamard
+qc.h(0)
+print(qc.draw('text'))
